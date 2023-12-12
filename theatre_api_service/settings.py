2b14fd10d3ab0777d23d9f463e0ed8b3277de0f4
Severyn Krok <sevkrok@gmail.com>
2023-12-12 21:14:30 +0200

    Change token lifetime

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -165,7 +165,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),  # TODO: change to `minutes=5`
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
