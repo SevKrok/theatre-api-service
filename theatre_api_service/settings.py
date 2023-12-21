8424251a6e1256c3727759a3c25269f5c574f48b
Severyn Krok <sevkrok@gmail.com>
2023-12-21 14:56:00 +0200

    Add DEBUG to .env

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = []
 
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Additional aps
     "debug_toolbar",
     "rest_framework",
     "rest_framework_simplejwt",
